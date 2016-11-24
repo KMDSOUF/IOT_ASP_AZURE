69c2fbb381f62bb586169e7fbd7c13eb0b7e1cca
KMDSOUF <kamad.soufiane@gmail.com>
2016-11-24 16:29:25 +0100

    Creation du projet ASP.NET MVC

---
+++ Yourlake_Azure/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Yourlake_Azure.MvcApplication" Language="C#" %>
---
+++ Yourlake_Azure/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Yourlake_Azure.Startup))]
+namespace Yourlake_Azure
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
