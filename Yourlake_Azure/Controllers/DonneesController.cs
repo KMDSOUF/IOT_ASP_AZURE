08d8bc49ba2be2cc755f6caa822cf58d63cb8f4e
KMDSOUF <kamad.soufiane@gmail.com>
2016-11-27 15:35:26 +0100

    Ajout de: HtmlAgilityPack;

--- Yourlake_Azure/Controllers/DonneesController.cs
+++ Yourlake_Azure/Controllers/DonneesController.cs
@@ -4,16 +4,22 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Yourlake_Azure.Context;
 using Yourlake_Azure.Models;
+using HtmlAgilityPack;
+using System.Text;
 
 namespace Yourlake_Azure.Controllers
 {
     public class DonneesController : Controller
     {
         private Context.Context db = new Context.Context();
+        //liste pour stocker les donner 
+        public static List<Donnee> listeDonnee = new List<Donnee>();
 
         // GET: Donnees
         public ActionResult Index()
@@ -61,7 +67,96 @@ namespace Yourlake_Azure.Controllers
             ViewBag.RegionId = new SelectList(db.Regions, "RegionId", "Nom", donnee.RegionId);
             return View(donnee);
         }
-        
+
+        public void charger()
+        {
+
+        }
+
+        private static async Task<HttpResponseMessage> MakeRequest()
+        {
+            var httpClient = new HttpClient();
+            return await httpClient.GetAsync(new Uri("http://requestb.in/1e6juns1?inspect"));
+        }
+
+        private static string getPage()
+        {
+            string html;
+            // obtain some arbitrary html....
+            using (var client = new WebClient())
+            {
+                html = client.DownloadString("http://requestb.in/16h4ht11?inspect");
+            }
+            // use the html agility pack: http://www.codeplex.com/htmlagilitypack
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            StringBuilder sb = new StringBuilder();
+            foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//pre[@class='body prettyprint']"))
+            {
+                sb.AppendLine(node.InnerText);
+            }
+            string final = sb.ToString();
+
+            return final;
+        }
+
+        private static List<Donnee> getDataFromPageThenStoreItToList(string page)
+        {
+            //decouper les chaines dans le fichier
+            string[] lines = page.Split(new string[] { "&amp;", "None", "\n" }, StringSplitOptions.None);
+
+            Donnee d = new Donnee();
+
+            //dictionnaire contenant les cles values
+            //Dictionary<string, string> dictionnaire = new Dictionary<string, string>();
+            foreach (string line in lines)
+            {
+                try
+                {
+                    string[] pair = line.Split(new[] { '=' });
+                    string key = pair[0];
+                    string val = pair[1];
+                    Console.WriteLine(key + " : " + val);
+
+                    switch (key)
+                    {
+                        case ("slot_temperature"):
+                            d.Temperature = val;
+                            break;
+                        case ("slot_debit"):
+                            d.Debit = val;
+                            break;
+                        case ("data"):
+                            break;
+                        case ("slot_humidite_eau"):
+                            d.Humidite_eau = val;
+                            break;
+                        case ("slot_humidite"):
+                            d.Humidite = val;
+                            break;
+                        case ("time"):
+                            d.Time = val;
+                            break;
+                        case ("device"):
+                            break;
+                        case ("signal"):
+                            break;
+                    }
+                    if (d.Temperature != "" && d.Debit != "" && d.Humidite_eau != "" && d.Humidite != "" && d.Time != "")
+                    {
+                        listeDonnee.Add(d);
+                        // new Donnee pour réutiliser l'objet d avec des attributs null
+                        d = new Donnee();
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+
+            }
+            return listeDonnee;
+        }
+
 
         protected override void Dispose(bool disposing)
         {
